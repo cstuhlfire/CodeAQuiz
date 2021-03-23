4f70cf79f639e592fa76f9a81562257661f3a8dd
Coleen <79562228+cstuhlfire@users.noreply.github.com>
2021-03-23 00:06:42 -0500

    add/ Added dynamic build of welcome screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,103 +4,119 @@
 
 //*** Global declarations
 
-// Create question objects
+// Create question bank with an array of objects
 let questionBank = [
   {
     question: "Who is Jerry's neighbor across the hall?",
     answers: ["Elaine", "George", "Kramer", "Banyon"],
-    correctAnswer: 3
+    correctAnswer: 3,
   },
   {
     question: "Who is Jerry's nemisis?",
     answers: ["Newman", "Mr. Pit", "Kramer", "Banyon"],
-    correctAnswer: 1
+    correctAnswer: 1,
   },
   {
     question: "What is Elaine's favorite order from The Resaurant?",
     answers: ["Grilled Cheese", "Tuna on Rye", "Cereal", "Big Salad"],
-    correctAnswer: 4
+    correctAnswer: 4,
   },
   {
     question: "What winter holiday does Frank Costanza celebrate?",
     answers: ["Christmas", "Festivus", "Dark Solstice", "Hanukkah"],
-    correctAnswer: 2
+    correctAnswer: 2,
   },
   {
     question: "What is Kramer's first name?",
     answers: ["Frank", "Kranston", "Cosmo", "Charlie"],
-    correctAnswer: 3
-  }
+    correctAnswer: 3,
+  },
+];
+
+// Create welcome array
+let welcomeArray = [
+  "Answer each question by clicking on an option.",
+  "You will start with 60 seconds.",
+  "Incorrect answers lose 10 seconds.",
+  "Try to answer all the questions before time runs out!",
 ];
 
 // Variables
 let totalQuestions = questionBank.length;
-let totalTime = 25;
+let totalTime = 5;
 let secondsLeft = totalTime;
 let timerInterval = 0;
 
+// Query selectors
 let timerDisplay = document.querySelector("#timer");
 let startButton = document.querySelector("#start");
-let container = document.querySelector(".container");
+let containerMain = document.querySelector(".containers");
+let container1 = document.querySelector(".container1");
+let containerHeader = document.querySelector(".container-header");
+let listItems = document.querySelectorAll("li");
 
 //*** Javascript and function calls
+resetValues();
+setWelcome();
 
 //*** Function definitions
+function setWelcome() {
+  containerHeader.textContent = "Directions:";
 
-// Reset values to welcome screen defaults
-function resetValues() {
-    secondsLeft = totalTime;
-    startButton.setAttribute("style", "visibility:visible")
-    container.setAttribute("style", "visiblity:visible");
+  for (let i = 0; i < listItems.length; i++) {
+    const element = listItems[i];
+    element.textContent = welcomeArray[i];
+  }
 }
 
-// Set timer interval with set Interval
+function resetValues() {
+  secondsLeft = totalTime;
+  timerDisplay.textContent = "Timer: " + secondsLeft;
+  //containerMain.setAttribute("style", "visiblity:visible");
+}
+
 function countDown() {
   timerInterval = setInterval(function () {
     secondsLeft--;
-   
     timerDisplay.textContent = "Timer: " + secondsLeft;
+
     if (secondsLeft === 0) {
       // Stops execution of action at set interval
       clearInterval(timerInterval);
-      resetValues(); 
     }
   }, 1000);
 }
 
-// The following function renders questions as <li> elements
 function renderQuestions() {
-    clearInterval(timerInterval);
-    // startButton.setAttribute("style", "visibility:hidden");
-    // container.setAttribute("style", "visibility:hidden");
-    countDown();
-    console.log(questionBank.length);
-    console.log(questionBank);
+  clearInterval(timerInterval);
+  startButton.setAttribute("style", "visibility:hidden");
+  countDown();
+  //setQuestions();
+  
 
-    // secondsLeft = secondsLeft - 10;
-    
-    // Clear todoList element and update todoCountSpan
-    // todoList.innerHTML = "";
-    // todoCountSpan.textContent = todos.length;
-  
-    // Render a new li for each answer
-    // for (var i = 0; i < todos.length; i++) {
-    //   var todo = todos[i];
-  
-    //   var li = document.createElement("li");
-    //   li.textContent = todo;
-    //   li.setAttribute("data-index", i);
-  
-    //   var button = document.createElement("button");
-    //   button.textContent = "Complete ✔️";
-  
-    //   li.appendChild(button);
-    //   todoList.appendChild(li);
-    //}
-  }
+  // secondsLeft = secondsLeft - 10;
+
+  // Clear todoList element and update todoCountSpan
+  // todoList.innerHTML = "";
+  // todoCountSpan.textContent = todos.length;
+
+  // Render a new li for each answer
+  // for (var i = 0; i < todos.length; i++) {
+  //   var todo = todos[i];
+
+  //   var li = document.createElement("li");
+  //   li.textContent = todo;
+  //   li.setAttribute("data-index", i);
+
+  //   var button = document.createElement("button");
+  //   button.textContent = "Complete ✔️";
+
+  //   li.appendChild(button);
+  //   todoList.appendChild(li);
+  //}
+}
 
 //*** Event listeners
-// Listen for a click event on start button
 startButton.addEventListener("click", renderQuestions);
 
 // Create title ***
@@ -112,7 +128,7 @@ startButton.addEventListener("click", renderQuestions);
 // Display countdown ***
 
 // Create question bank object ***
-// Create question array
+// Create question array ***
 // Read question array for 5 questions
 // For each question display question and create a li to display answer choices
 // Check if correct
